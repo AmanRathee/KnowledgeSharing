a2a17cd2ce5f34d661a16f625809c85427d2d369
Aman Rathee <aman.rathee@onsolve.com>
2023-05-07 16:23:35 +0530

    httpretry

---
+++ KnowledgeSharing/HttpRetry/HttpRetry.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+public static class RetryHelper
+{
+    public static async Task RetryOnExceptionAsync(int retryTimes, TimeSpan retryDelay, Func<Task> operation)
+    {
+        for (int i = 0; i < retryTimes; i++)
+        {
+            try
+            {
+                await operation();
+                return;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Transient failure occurred, retrying in {retryDelay.TotalSeconds} seconds ({i + 1}/{retryTimes}): {ex.Message}");
+                await Task.Delay(retryDelay);
+            }
+        }
+        throw new Exception($"Failed after {retryTimes} retries.");
+    }
+}
---
+++ KnowledgeSharing/HttpRetry/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static async Task Main(string[] args)
+    {
+        await RetryHelper.RetryOnExceptionAsync(3, TimeSpan.FromSeconds(1), async () =>
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                HttpResponseMessage response = await httpClient.GetAsync("https://example11111.com/api/endpoint");
+                response.EnsureSuccessStatusCode();
+                string result = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(result);
+            }
+        });
+    }
+}
