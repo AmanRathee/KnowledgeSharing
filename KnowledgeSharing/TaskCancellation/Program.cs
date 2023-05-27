0addbd9d0a41bd9e07b2dd337d92152221b4bbeb
Aman Rathee <aman.rathee@onsolve.com>
2023-05-27 22:08:22 +0530

    task cancellation

---
+++ KnowledgeSharing/TaskCancellation/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static readonly CancellationTokenSource cts = new CancellationTokenSource();
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Execution started.");
+        try
+        {
+            cts.CancelAfter(1000);
+            await OperationAsync(cts.Token);
+        }
+        catch (OperationCanceledException)
+        {
+            Console.WriteLine("\nTasks cancelled: timed out.\n");
+        }
+        finally
+        {
+            cts.Dispose();
+        }
+        Console.WriteLine("Execution ending.");
+    }
+    private static async Task OperationAsync(CancellationToken token)
+    {
+        await Task.Delay(4000, token);
+    }
+}
